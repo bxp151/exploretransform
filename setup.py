5cb26733d2d799b16514a5ba5d1da17e18cd22f4
Brian <bjpietra@ncsu.edu>
2020-10-05 13:25:54 -0400

    step before testing on test PYPI

--- setup.py
+++ setup.py
@@ -7,8 +7,14 @@ Created on Sun Sep 27 11:00:23 2020
 """
 import setuptools 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 # This call to setup() does all the work
 setuptools.setup(
@@ -19,12 +25,15 @@ setuptools.setup(
     description="Explore and transform your data",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    include_package_data=True,
     url="https://github.com/bxp151/exploretransform",
-    packages=setuptools.find_packages(),
+    py_module =['exploretransform'],
+    install_requires=['numpy', 'pandas', 'plotnine', 'scipy', 'sklearn',
+                      'minepy', 'dcor'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_required = '>=3.6.9'
+    python_requires = '>=3.6.9'
 )
